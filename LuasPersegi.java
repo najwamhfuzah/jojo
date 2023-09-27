f6bc273240fc9012946e6c3adb8440b0bad1d520
najwamhfuzah <146162831+najwamhfuzah@users.noreply.github.com>
2023-09-27 12:56:10 +0700

    Add files via upload

---
+++ LuasPersegi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedataa;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LuasPersegi {
+    public static void main(String[] args) {
+        int sisi = 45;
+        int hasil = sisi*sisi;
+        System.out.println("hasil : " + hasil);
+    }
+}
